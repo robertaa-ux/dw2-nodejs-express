8f13cf4927900cd602d64563bed80001194d16b7
DSM2 <fatec-dsm2@academico.intra>
2026-04-09 18:54:54 -0300

    Aula 07 - CRUD

---
+++ nodejs-aula-07-CRUD/SistemaLoja/models/Cliente.js
@@ -0,0 +1,23 @@
+import Sequelize from "sequelize";
+import connection from "../config/sequelize-config.js";
+
+//Criando o Model
+//O método define cria uma tabela no Banco de Dados
+const Cliente = connection.define('clientes', {
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    cpf: {
+         type: Sequelize.STRING,
+         allowNull: false,
+    },
+    endereco: {
+         type: Sequelize.STRING,
+         allowNull: false,
+    },
+});
+//O método sync() sincroniza os dados com o banco
+// force: false: não recria a tabela caso ela já exista
+Cliente.sync({force: false});
+export default Cliente;
